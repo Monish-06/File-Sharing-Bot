b490dd2b42d3c22772c41c0894c03a83b5cd2c7c
Monish-06 <70092044+Monish-06@users.noreply.github.com>
2025-04-06 18:18:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,5 @@
 #(©)Codexbotz
 
-import threading
-import requests
-import time
- 
-def keep_alive():
-    while True:
-        try:
-            requests.get("https://famous-addi-moxibeatz21-7ed6f21b.koyeb.app/")
-        except:
-            pass
-        time.sleep(90)  # Ping every 5 minutes
- 
-threading.Thread(target=keep_alive, daemon=True).start()
-
 
 
 from aiohttp import web
