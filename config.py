9d41df70c2b43e31785041eb512418bd30101919
Monish-06 <70092044+Monish-06@users.noreply.github.com>
2025-04-06 21:07:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -30,8 +30,8 @@ DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://monish280720:hsUe1KPZd5wh
 DB_NAME = os.environ.get("DATABASE_NAME", "filestore")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0")
-JOIN_REQUEST_ENABLE = os.environ.get("JOIN_REQUEST_ENABLED", None)
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002643721771")
+JOIN_REQUEST_ENABLE = os.environ.get("JOIN_REQUEST_ENABLED", False)
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
